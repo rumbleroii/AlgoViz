70a92796b0bd4cb62904741e9be2a39a6563f7b1
Rithik Marudappa <57358233+rumbleroii@users.noreply.github.com>
2022-11-26 18:06:01 +0530

    Google Auth Done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const getToken = () => {
+  const token = sessionStorage.getItem("sessionToken");
+  return token;
+};
+
+const getDetails = async () => {
+  try {
+    const sessionToken = getToken();
+
+    const res = await axios.get(
+      "https://www.googleapis.com/oauth2/v1/tokeninfo",
+      {
+        params: { id_token: `${sessionToken}` },
+      }
+    );
+
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const Home = () => {
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    getDetails().then((res) => setDetails(res));
+  }, []);
+
   return (
     <div className="container-md mx-auto">
-      <h1>Welcome to AlgoViz</h1>
+      <h1>Welcome to AlgoViz {details.email}</h1>
     </div>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,50 @@
-import React from "react";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
+import { useNavigate, redirect } from "react-router-dom";
+
+const getToken = () => {
+  const token = sessionStorage.getItem("sessionToken");
+  return token;
+};
+const setToken = (res) => {
+  console.log(res);
+  sessionStorage.setItem("sessionToken", res.credential);
+};
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const onSuccess = async (res) => {
+    try {
+      setToken(res);
+      navigate("/home");
+    } catch (err) {
+      redirect("/");
+    }
+  };
+
+  const onError = (err) => console.log(err);
+
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="m-2">Welcome to AlgoViz</h1>
-      <br></br>
-      <h1>Login</h1>
-    </div>
+    <>
+      <div className="h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Welcome To AlgoViz
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600 pb-5">
+              Login To Your Account
+            </p>
+            <div className="flex justify-center">
+              <GoogleOAuthProvider clientId="686237414426-i403mqs4n53kj8n3e7m59nobp91dks41.apps.googleusercontent.com">
+                <GoogleLogin onSuccess={onSuccess} onError={onError} />
+              </GoogleOAuthProvider>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
