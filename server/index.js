7e40ae890ccf5eaf26efd148f2802cafc3fd3801
Rithik Marudappa <57358233+rumbleroii@users.noreply.github.com>
2022-11-27 18:18:32 +0530

    DB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const connectDB = require("./middleware/dbconnect");
 
 const cors = require("cors");
 
 // Init Middleware
 cors();
+
+// Connect Database
+connectDB();
 
 // Port
 const PORT = process.env.PORT || 3000;
@@ -14,7 +18,24 @@ app.get("/", (req, res) => {
   res.send("Server works");
 });
 
+// Routes
+app.get("/api/auth", require("./routes/auth"));
+
+app.get("*", async (req, res) => {
+  try {
+    console.log("Undefined Route Accessed");
+    res.status(404).json({
+      msg: "Route not found",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Server Error",
+    });
+  }
+});
+
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT}`);
+  console.log(`[INFO] Server Running on Port ${PORT}`);
 });
---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+
+router.post("/login", (req, res) => {
+    const { email, name } = req.body();
+
+
+})
+
+
+module.exports = router;
