6c02d2dceb0a74eb85fdbb4987b1693ab38df42d
IIRobotNinjaII <57091026+IIRobotNinjaII@users.noreply.github.com>
2022-11-27 22:14:42 +0530

    Revert "updated faq"

--- server/routes/posts.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const Post = require("../models/Post");
-
-router.get("/",async (req,res) => {
-    const posts = await Post.find()
-    res.send(posts)
-})
-module.exports = router
